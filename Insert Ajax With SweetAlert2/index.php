bedbbc9c7228effa5ad9a55c259ed1938a212dc6
LKZ <lucas_vieira97@github.com>
2018-12-13 14:52:39 -0200

    update

--- Insert Ajax With SweetAlert2/index.php
+++ Insert Ajax With SweetAlert2/index.php
@@ -6,11 +6,6 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        <script
-            src="https://code.jquery.com/jquery-3.3.1.js"
-            integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
-            crossorigin="anonymous">
-        </script>
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@7.32.2/dist/sweetalert2.all.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@7.32.2/package.json"></script>
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@7.32.2/sweetalert2.d.ts"></script>
