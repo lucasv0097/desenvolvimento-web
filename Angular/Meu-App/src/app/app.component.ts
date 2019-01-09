e130ba311cf69eba9921452caa99de4dd46f717c
lucasv0097 <lucas_vieira97@hotmail.com>
2019-01-09 18:35:18 -0200

    SweetAlert

--- Angular/Meu-App/src/app/app.component.ts
+++ Angular/Meu-App/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Meu-App';
+
+  alert() {
+    Swal({
+      type: 'error',
+      title: 'Oops...',
+      text: 'Something went wrong!',
+      footer: '<a href>Why do I have this issue?</a>'
+    });
+  }
 }
