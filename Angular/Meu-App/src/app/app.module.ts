95bef21b5bbadfa503bd44da331cf32cb3664998
lucasv0097 <lucas_vieira97@hotmail.com>
2019-01-09 18:24:59 -0200

    formlogin

--- Angular/Meu-App/src/app/app.module.ts
+++ Angular/Meu-App/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormloginComponent } from './formlogin/formlogin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormloginComponent
   ],
   imports: [
     BrowserModule,
