968e2e382c28fada69a7e26ae40dd6bcff376278
lucasv0097 <lucas_vieira97@hotmail.com>
2019-03-20 18:38:43 -0300

    add bootstrap

--- Angular/MeuApp/src/app/app.module.ts
+++ Angular/MeuApp/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 @NgModule({
   declarations: [
@@ -12,6 +15,9 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    BsDropdownModule,
+    TooltipModule,
+    ModalModule
   ],
   providers: [],
   bootstrap: [AppComponent]
