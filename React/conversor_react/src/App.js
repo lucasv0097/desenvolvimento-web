c8db2bf9604c9343db62553ba2874f02584164f4
LKZ <32254319+lucasv0097@users.noreply.github.com>
2019-04-14 19:39:33 -0300

    conversor_moedas
    webApp with react that converter currencies with ( free converter currency api )

---
+++ React/conversor_react/src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Conversor from "./Components/Conversor";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+        <h1>conversor de moedas</h1>
+        <div className = "linha">  
+
+          <Conversor moedaA = "USD" moedaB = "BRL"></Conversor>
+          <Conversor moedaA = "BRL" moedaB = "USD"></Conversor>
+        </div>
+
+        <div className = "linha">
+          
+          <Conversor moedaA = "CAD" moedaB = "BRL"></Conversor>
+          <Conversor moedaA = "BRL" moedaB = "CAD"></Conversor>
+        </div>
+
+        <div className = "linha">
+          
+          <Conversor moedaA = "EUR" moedaB = "BRL"></Conversor>
+          <Conversor moedaA = "BRL" moedaB = "EUR"></Conversor>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
